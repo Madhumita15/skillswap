d7995282c813b7fa9cd59ce6079bba17ad0a9726
Shilpa Singh <shilpasingh10101@gmail.com>
2026-09-11 17:06:53 +0530

    changes on skills

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,10 @@ const app = express()
 app.get("/", (req,res)=>{
     console.log("Our Group Project SwapSkills")
 })
+
+app.get("/skill",(req,res)=>{
+    console.log("skills is starting")
+})
 const PORT = 3007;
 app.listen(PORT, ()=>{
     console.log(`app is listening on PORT ${PORT}`)
